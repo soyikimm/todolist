c0bcccb6e74f1d6a339f90b454591c08dcf00056
soyikimm <flamingo7763@gmail.com>
2021-10-10 20:05:17 +0900

    comment update

--- js/todo.js
+++ js/todo.js
@@ -6,34 +6,34 @@ const TODOS_KEY = "todos";
 
 let toDos = [];
 
-// To do list 저장
+// To do list 로컬 스토리지에 저장
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 // To do list 삭제
 function deleteToDo(event) {
-  const li = event.target.parentElement;
-  console.log(li.id);
-  li.remove();
+  const li = event.target.parentElement; // parentElement는 event.target의 부모를 찾아줌. 어떤 li를 클릭했는지 알수있음.
+  li.remove(); //Button의 부모는 li임. 클릭한 li 지우기
   toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   saveToDos();
 }
 
-// To do list 보여주기
+// To do list 저장해서 보여주기
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo.text; //span의 텍스트는 handleToDoSubmit에서 사용자 준 newTodo값이 됨
-  const button = document.createElement("button");
+  span.innerText = newTodo.text; //span의 텍스트는 handleToDoSubmit에서 사용자 준 newTodo값이 됨. 투두리스트가 여기들어감
+  const button = document.createElement("button"); //삭제버튼
   button.innerText = "X";
-  button.addEventListener("click", deleteToDo);
+  button.addEventListener("click", deleteToDo); //버튼이 클릭되면 deleteTodo를 실행 (15번)
   li.appendChild(span); //li는 span이라는 자식을 갖게됨으로 li안에 span있음
   li.appendChild(button);
-  toDoList.appendChild(li); //list로 출력해줌
+  toDoList.appendChild(li); //<button>을 <li>로 감싸며 출력해줌
 }
 
+//To do list 추가
 function handleToDoSubmit(event) {
   event.preventDefault(); //엔터를 쳤을 때 submit의 기본동작인 페이지 새로고침 막기
   const newTodo = toDoInput.value;  //input의 현재 value를 newTodo라는 새로운 변수에 복사
@@ -42,13 +42,13 @@ function handleToDoSubmit(event) {
     text: newTodo,
     id: Date.now(),
   };
-  toDos.push(newTodoObj);
+  toDos.push(newTodoObj); //toDos array를 가져와서 newTodo를 Push
   paintToDo(newTodoObj);
   saveToDos();
 }
 
-toDoForm.addEventListener("submit", handleToDoSubmit);
-const savedToDos = localStorage.getItem(TODOS_KEY);
+toDoForm.addEventListener("submit", handleToDoSubmit); //사용자가 form을 submit하면 빈값을 넣고 newTodo를 toDos array에 push
+const savedToDos = localStorage.getItem(TODOS_KEY); //로컬에 투두리스트저장
 
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
--- js/weather.js
+++ js/weather.js
@@ -2,19 +2,19 @@ const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
 const API_KEY = "ece3b30096da950049fee9dbc5691cd3";
 
-function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-      .then((response) => response.json())
+function onGeoOk(position) { //모든게 잘될때 실행될 함수
+    const lat = position.coords.latitude; //위도
+    const lon = position.coords.longitude; //경도
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`; 
+    //숫자들을 장소로 바꿔줄 API서비스. 섭씨는 units=metric사용
+    fetch(url) //fetch로 API통신. 서버가 응답할때가지 기다리지않으면 값을 못받음
+      .then((response) => response.json()) //.than()을 사용하여 json값을 넘겨받음
       .then((data) => {
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main}/${data.main.temp}℃/`;
+        city.innerText = data.name; //data.name:도시이름
+        weather.innerText = `${data.weather[0].main}/${data.main.temp}℃/`; 
       });
   }
-  function onGeoError() {
+  function onGeoError() { //에러가 발생했을때 실행될 함수
     alert("Can't find you. No weather for you.");
   }
-  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError); //이 함수는 user의 위치를 줌.(success, error)
